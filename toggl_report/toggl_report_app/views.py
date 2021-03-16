fdfbcdb41e28176e8d59c2246dbd5a442d34025a
naokodama <loaf.m.wind5420@gmail.com>
2021-03-17 00:02:12 +0900

    solve the name error.

--- toggl_report/toggl_report_app/views.py
+++ toggl_report/toggl_report_app/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponce
+from django.http import HttpResponse
 
 def index(request):
-    return HttpResponce("Hello, world. You're at the toggl_report_app index.")
-
-# Create your views here.
+    return HttpResponse("Hello, world. You're at the toggl_report_app index.")
