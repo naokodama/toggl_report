ae38e335655dd4ee7957ba4dde2fec017ec06f28
naokodama <loaf.m.wind5420@gmail.com>
2021-07-09 23:39:45 +0900

    show the circle graph.

--- toggl_report/toggl_report_app/urls.py
+++ toggl_report/toggl_report_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.UserView.as_view(), name='user_view'),
     path('<str:pk>/daily_report/', views.DailyView.as_view(), name='daily_report'),
     path('<str:user_id>/daily_report/<str:date>/', views.daily_view, name='daily_report_view'),
+    path('<str:user_id>/daily_report/<str:date>/circle_graph', views.showCircleGraph, name='show_circle_graph'),
     ]
 
--- toggl_report/toggl_report_app/views.py
+++ toggl_report/toggl_report_app/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
+import matplotlib.pyplot as plt
+import io
 
 from .models import TogglUser
 
@@ -32,6 +34,32 @@ def daily_view(request, user_id, date):
     result = requests.get('https://www.toggl.com/api/v8/workspaces', auth = (user_info.api_token, 'api_token'))
     data = result.json()
     Data = data[0]
+    params = {
+        'user_agent': user_info.mail,
+        'workspace_id': Data['id'],
+        'since': date,
+        'until': date,
+    }
+    r = requests.get('https://toggl.com/reports/api/v2/details',
+                     auth=HTTPBasicAuth(user_info.api_token, 'api_token'),
+                     params=params)
+    json_r = r.json()
+    context = {'workspace_id' : Data['id'], 'report_json' : json_r['data'], 'date': date, 'user_id': user_id}
+
+    return render(request, 'toggl_report_app/daily_report.html', context)
+
+def plt2png():
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', dpi=200)
+    png_data = buf.getvalue()
+    buf.close()
+    return png_data
+
+def getTogglData(user_id, date):
+    user_info = get_object_or_404(TogglUser, pk = user_id)
+    result = requests.get('https://www.toggl.com/api/v8/workspaces', auth = (user_info.api_token, 'api_token'))
+    data = result.json()
+    Data = data[0]
     context = {'workspace_id' : Data['id']}
     params = {
         'user_agent': user_info.mail,
@@ -43,6 +71,24 @@ def daily_view(request, user_id, date):
                      auth=HTTPBasicAuth(user_info.api_token, 'api_token'),
                      params=params)
     json_r = r.json()
-    context = {'workspace_id' : Data['id'], 'report_json' : json_r}
+    return json_r['data']
 
-    return render(request, 'toggl_report_app/daily_report.html', context)
+def showCircleGraph(request, user_id, date):
+    toggl_data = getTogglData(user_id, date)
+    labels = []
+    sizes = []
+    for tdata in toggl_data:
+        labels.append(tdata['description'])
+        sizes.append(tdata['dur'])
+
+    fig, ax = plt.subplots()
+    patches, texts, autotexts = ax.pie(sizes, labels=labels, autopct='%1.1f%%',
+                                       shadow=True, startangle=90, labeldistance=0.7, pctdistance=0.5, wedgeprops={'linewidth': 1, 'edgecolor':"0.8"})
+    ax.axis('equal') # 面積比=割合
+    plt.rcParams['font.family'] = 'MS Gothic'
+    plt.setp(autotexts, size=12)
+    png_data = plt2png()
+    plt.cla()
+    response = HttpResponse(png_data, content_type='image/png')
+    return response
+
