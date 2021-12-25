1931a38e983a5b124d0b82745f731a4890a7bdaa
naokodama <loaf.m.wind5420@gmail.com>
2021-12-26 04:02:52 +0900

    modified test_models.py.

--- toggl_report/toggl_report_app/tests/test_models.py
+++ toggl_report/toggl_report_app/tests/test_models.py
@@ -2,9 +2,7 @@ from django.test import TestCase
 from toggl_report_app.models import TogglUser
 from toggl_report_app.models import SelectedUser
 
-class TogglUserTests(TestCase):
-
 class SelectedUserTest(TestCase):
-    def test_not_exist_selected_user():
-        selected_user = SelectedUser.object.all()
-        self.assertEqual(select_user.count(). 0)
+    def test_not_exist_selected_user(self):
+        selected_user = SelectedUser.objects.all()
+        self.assertEqual(selected_user.count(), 0)
